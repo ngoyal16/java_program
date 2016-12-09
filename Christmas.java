22f01d82200f0829dad1340cc3b6dc75c5985be6
Nitin Goyal <nitingoyal.dev@gmail.com>
2016-12-09 23:26:26 +0530

    Path Updates

--- Christmas.java
+++ Christmas.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class Christmas {
 
-	static String path = "----path goes here----";
+	static String path = "items.txt";
 	static File f = new File(path);
 	static String absolutePath = f.getAbsolutePath();
 	static ArrayList<String> records = new ArrayList<>();
@@ -19,11 +19,22 @@ public class Christmas {
 		System.out.println("*************************************************************");
 		System.out.println("*                          WELCOME                          *");
 		System.out.println("*************************************************************");
-		System.out.println();
+		
+		if(f.exists()){
+			FileReader fr = new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
+			String line  = new String();
+			try {
+				while((line = br.readLine()) != null){
+					records.add(line);
+				}
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	static void menu(){
-		
 		System.out.println();
 		System.out.println("Please choose how you wish to proceed -");
 		System.out.println("1. Enter Christmas Shopping List Records");
@@ -32,31 +43,26 @@ public class Christmas {
 		System.out.println("4. Read the Christmas Shopping List Records from a File");
 		System.out.println("5. Display All Christmas Shopping List Records");
 		System.out.println("6. Exit the Application");
-		System.out.println();
+		System.out.print("Enter Your Choice : ");
 	}
 	
 	static void close(){
-		
 		System.out.println("*************************************************************");
 		System.out.println("*           “MERRY CHRISTMAS AND HAPPY NEW YEAR!”           *");
 		System.out.println("*************************************************************");
 	}
 	
 	static void newRecord(String rec){
-		
 		String[] r = rec.split(",");
 		for(String re : r)
 		records.add(re.trim() + " | Not Purchased");
 	}
 	
 	static void writeRecords(){
-		
-		
 		FileWriter fw = null;
 		PrintWriter pw = null;
 		try{
-			if(!f.exists())
-				f.createNewFile();
+			if(!f.exists()) f.createNewFile();
 			fw = new FileWriter(f);
 			pw = new PrintWriter(fw);
 			for(String rec : records){
@@ -86,7 +92,6 @@ public class Christmas {
 	}
 	
 	static void readRecords(){
-		
 		FileReader fr = null;
 		try {
 			fr = new FileReader(f);
@@ -115,65 +120,49 @@ public class Christmas {
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		welcome();
-		if(f.exists()){
-			FileReader fr = new FileReader(f);
-			BufferedReader br = new BufferedReader(fr);
-			String line  = new String();
-			try {
-				while((line = br.readLine()) != null){
-					records.add(line);
-				}
-			}catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
 		Scanner sc = new Scanner(System.in);
 		
-		while(true){
+		welcome();
+	
+		do{
 			menu();
-			String choice = sc.nextLine();
 			
+			String choice = sc.nextLine();
 			switch(choice){
-				
 				case "1" :
 					System.out.println("Enter new item or items separated by comma \", \"");
 					newRecord(sc.nextLine());
 					break;
+					
 				case "2" :
 					System.out.println("List the item you have purchased");
 					updatePurchase(sc.nextLine());
 					break;
+					
 				case "3" :
 					writeRecords();
 					break;
+					
 				case "4" :
-					System.out.println("Do you wish to read from file? Discard changes, if any?");
-					System.out.println("y / n ?");
+					System.out.print("Do you wish to read from file before saveing it will Discard changes, if any? (y/n) : ");
 					String c = sc.next();
-					if(c.equals("y")){
+					if(c.equals("y") || c.equals("Y")){
 						readRecords();
 					}
-					else{
-						System.out.println("Please save your changes using choice below");
-					}
-					
-					
 					break;
+					
 				case "5" :
 					display();
 					break;
-				case "6" : 
-					close();
-					sc.close();
-					return;
+				case "6": 
+				    close();
+				    sc.close();
+				    return;
+					
 				default :
 					System.out.println("Please Enter a valid choice");
 			}
-		}
+		}while(true);
+		
 	}
 }
-
-
